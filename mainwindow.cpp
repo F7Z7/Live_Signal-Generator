49a53528d0e96855d0348ad47a90d43a0e9cfb99
farza <farzanrichu610@gmai.com>
2025-06-04 03:16:32 +0530

    added some functions:mainWindow.cpp and header

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "qcustomplot.h"
 #include <QComboBox>
+#include <QTimer>
+#include <QMessageBox>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,19 +12,46 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui->signalDrop->addItems({"Sine","Square","Triangle","SawTooth"});
+    timer= new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &MainWindow::update_plot);
+    ui->graphPlotter->addGraph();
+    ui->graphPlotter->graph(0)->setData(X,Y);
+    ui->graphPlotter->graph(0)->setPen(QPen(Qt::blue));
+    ui->graphPlotter->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20)));
+    ui->graphPlotter->xAxis->setRange(0, 1);
+    ui->graphPlotter->yAxis->setRange(-1.5, 1.5);
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+void MainWindow::generatePlot(const QString &type, double freq){
+    int sampleRate = 1000;
+    int N = 1000;
+    double dt = 1.0/sampleRate; //sampled evry second
 
+}
 void MainWindow::on_startPlot_clicked()
 {
 
+    QString signalType =ui->signalDrop->currentText();
+   freq=ui->freqInput->value();
+
+    if(freq<=0){
+        QMessageBox::warning(this, "Invalid Input", "Frequency can't be negative or zero!");
+
+    }
+   generatePlot(signalType,freq);
+    timer->start(30);
+
 }
 
+void MainWindow::update_plot(){
 
+}
 void MainWindow::on_stopPlot_clicked()
 {
 
--- mainwindow.h
+++ mainwindow.h
@@ -21,10 +21,15 @@ private slots:
     void on_startPlot_clicked();
 
     void on_stopPlot_clicked();
-
+    void generatePlot(const QString &type, double freq);
+    void update_plot();
     void on_resetPlot_clicked();
 
 private:
+    //declaring necessary variables
     Ui::MainWindow *ui;
+    QTimer *timer;
+    double freq,t;
+    QVector<double> X,Y;
 };
 #endif // MAINWINDOW_H
